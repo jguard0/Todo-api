e0e4088e8b7139c3197ada844aecfcc9ff6f0232
James Guardino <jag@raadius.net>
2016-01-22 22:03:11 -0500

    Add variable playground js

---
+++ variable-playground.js
@@ -0,0 +1,33 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Samantha',
+// 	// 	age: 16
+// 	// }
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// array example
+var array = [ 15, 7 ];
+
+function pushgrade(obj, grade) {
+	obj.push(grade);
+}
+
+function creategrades(grades) {
+	grades =  [ 1,5,7 ];
+	debugger;
+}
+
+pushgrade(array,9);
+console.log(array);
+
+creategrades(array);
+console.log(array);
